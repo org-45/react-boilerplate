4daa76352602047df5a98d3afaa0d55dc513d80f
Krishna Dhakal <7krishna7dhakal7@gmail.com>
2023-11-08 12:57:59 +0545

    shadcn integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Button } from './components/ui/button'
+
 export default function App() {
   return (
-    <h1 className="text-4xl font-bold text-blue-800 text-center bg-gray-200 p-4 rounded-md shadow-md">
-      Hello, World!
-    </h1>
+    <div className="bg-gray-200 text-center p-4">
+      <h1 className="text-4xl font-bold text-blue-800 rounded-md shadow-md">
+        Hello, World!
+      </h1>
+      <Button className="mt-4" onClick={() => alert('Hello, World!')}>
+        Hello World
+      </Button>
+    </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   css: {
-    postcss: "./postcss.config.js",
+    postcss: './postcss.config.js',
   },
-});
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
