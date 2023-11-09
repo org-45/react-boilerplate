da683ef0c06411d9cfe774908ecc79e7b6572ec5
Krishna Dhakal <7krishna7dhakal7@gmail.com>
2023-11-09 17:56:20 +0545

    integrated admin dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/homepage'
 import Login from './pages/login'
+import AdminDashboard from './pages/admin'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<AdminDashboard />} />
       </Routes>
     </BrowserRouter>
   )
