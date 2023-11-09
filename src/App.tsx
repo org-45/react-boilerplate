6d34beac040d153959acd37a7820cc96b362585a
Krishna Dhakal <7krishna7dhakal7@gmail.com>
2023-11-09 16:33:23 +0545

    react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
-import Homepage from './pages/homepage'
-// import LoginPage from './pages/login'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/homepage'
+import Login from './pages/login'
 
-export default function App() {
-  return <Homepage />
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
+
+export default App
